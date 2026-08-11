a555c5e01773b05693421e938bbbad06b81b42c8
maidics <your-username@users.noreply.github.com>
2026-08-11 14:28:53 +0200

    feat: remove redirect from ResultTypes because it cannot be considered a solely positive outcome, add timeout type, and more.
    Furthermore removed unused methods, added status code for canceled and timeout type, added title to ToTypedResult helper, made the errors collection an IReadonlyList, refactored factory methods, made Result<T> sealed, renamed BusinessError type to RuleViolation.

--- src/VsaTemplate/Common/Models/Result.cs
+++ src/VsaTemplate/Common/Models/Result.cs
@@ -1,8 +1,6 @@
-using System.Collections.Frozen;
-
 namespace VsaTemplate.Common.Models;
 
-public class Result
+public record Result
 {
     protected Result(bool succeeded, IEnumerable<string> errors, ResultType type)
     {
@@ -12,95 +10,59 @@ public class Result
     }
 
     public bool Succeeded { get; }
-    public string[] Errors { get; }
+    public IReadOnlyList<string> Errors { get; }
     public ResultType Type { get; }
 
-    public static Result Success(ResultType type = ResultType.Success)
+    public static Result Success()
     {
-        if (type is ResultType.Redirect)
-            throw new InvalidOperationException(
-                $"'{ResultType.Redirect}' must be used with {nameof(Result)}<T>"
-            );
-
-        ThrowIfFailureType(type);
-
-        return new Result(true, [], type);
+        return new Result(true, [], ResultType.Success);
     }
 
-    public static Result<T> Success<T>(T value, ResultType successType = ResultType.Success)
+    public static Result<T> Success<T>(T value)
     {
         // method will check values
-        return Result<T>.Success(value, successType);
+        return Result<T>.Success(value);
     }
 
-    public static ResultFailure Failure(ResultType failureType, IEnumerable<string> errors)
-    {
-        return new ResultFailure(failureType, errors);
-    }
+    public static ResultFailure NotFound(params string[] errors) =>
+        new(ResultType.NotFound, errors);
 
-    public static ResultFailure NotFound(IEnumerable<string> errors)
-    {
-        return new ResultFailure(ResultType.NotFound, errors);
-    }
+    public static ResultFailure Conflict(params string[] errors) =>
+        new(ResultType.Conflict, errors);
 
-    public static ResultFailure Conflict(IEnumerable<string> errors)
-    {
-        return new ResultFailure(ResultType.Conflict, errors);
-    }
+    public static ResultFailure ExternalServiceError(params string[] errors) =>
+        new(ResultType.ExternalServiceError, errors);
 
-    public static ResultFailure ExternalServiceError(IEnumerable<string> errors)
-    {
-        return new ResultFailure(ResultType.ExternalServiceError, errors);
-    }
+    public static ResultFailure RuleViolation(params string[] errors) =>
+        new(ResultType.RuleViolation, errors);
 
-    public static ResultFailure BusinessError(IEnumerable<string> errors)
-    {
-        return new ResultFailure(ResultType.BusinessError, errors);
-    }
+    public static ResultFailure InternalError(params string[] errors) =>
+        new(ResultType.InternalError, errors);
 
-    public static ResultFailure InternalError(IEnumerable<string> errors)
-    {
-        return new ResultFailure(ResultType.InternalError, errors);
-    }
+    public static ResultFailure PaymentRequired(params string[] errors) =>
+        new(ResultType.PaymentRequired, errors);
 
-    public static ResultFailure PaymentRequired(IEnumerable<string> errors)
-    {
-        return new ResultFailure(ResultType.PaymentRequired, errors);
-    }
+    public static ResultFailure Unauthorized(params string[] errors) =>
+        new(ResultType.Unauthorized, errors);
 
-    public static ResultFailure Unauthorized(IEnumerable<string> errors)
-    {
-        return new ResultFailure(ResultType.Unauthorized, errors);
-    }
+    public static ResultFailure Forbidden(params string[] errors) =>
+        new(ResultType.Forbidden, errors);
 
-    public static ResultFailure Forbidden(IEnumerable<string> errors)
-    {
-        return new ResultFailure(ResultType.Forbidden, errors);
-    }
+    public static ResultFailure Canceled(params string[] errors) =>
+        new(ResultType.Canceled, errors);
 
-    public static ResultFailure Cancelled(IEnumerable<string> errors)
-    {
-        return new ResultFailure(ResultType.Cancelled, errors);
-    }
+    public static ResultFailure Timeout(params string[] errors) => new(ResultType.Timeout, errors);
 
     // IMPLICIT CONVERSION: ResultFailure => Result
     public static implicit operator Result(ResultFailure failure)
     {
         return new Result(false, failure.Errors, failure.Type);
     }
-
-    public static FrozenSet<ResultType> SuccessTypes => [ResultType.Success, ResultType.Redirect];
-
-    protected static void ThrowIfFailureType(ResultType type)
-    {
-        if (!SuccessTypes.Contains(type))
-            throw new InvalidOperationException($"'{type}' is not a success type.");
-    }
 }
 
-public class Result<T> : Result
+public sealed record Result<T> : Result
 {
-    private Result(bool succeeded, IEnumerable<string> errors, ResultType type, T value)
+    private Result(bool succeeded, string[] errors, ResultType type, T value)
         : base(succeeded, errors, type)
     {
         Value = value;
@@ -124,11 +86,8 @@ public class Result<T> : Result
         return new Result<T>(false, failure.Errors, failure.Type, default!);
     }
 
-    public static Result<T> Success(T value, ResultType successType = ResultType.Success)
+    public static Result<T> Success(T value)
     {
-        ThrowIfFailureType(successType);
-
-        ArgumentNullException.ThrowIfNull(value);
         return new Result<T>(true, [], ResultType.Success, value);
     }
 
@@ -141,7 +100,12 @@ public class Result<T> : Result
             );
         }
 
-        return new Result<TOther>(succeeded: false, errors: Errors, value: default!, type: Type);
+        return new Result<TOther>(
+            succeeded: false,
+            errors: Errors.ToArray(),
+            value: default!,
+            type: Type
+        );
     }
 }
 
@@ -150,31 +114,26 @@ public class ResultFailure
     public ResultType Type { get; }
     public string[] Errors { get; }
 
-    public ResultFailure(ResultType failureType, IEnumerable<string> errors)
+    public ResultFailure(ResultType type, params string[] errors)
     {
-        ThrowIfSuccessType(failureType);
-
-        Type = failureType;
-        Errors = errors.ToArray();
-    }
-
-    private static void ThrowIfSuccessType(ResultType type)
-    {
-        if (Result.SuccessTypes.Contains(type))
+        if (type == ResultType.Success)
             throw new InvalidOperationException($"'{type}' is not a failure type.");
+
+        Type = type;
+        Errors = errors.ToArray();
     }
 }
 
 public enum ResultType
 {
     Success, //Ok, NoContent
-    Redirect,
 
-    Cancelled,
+    Canceled, // Client closed connection
+    Timeout, // Gateway Timeout - can be named to ExternalServiceTimeout and also add another member: Request Timeout
     NotFound,
     Conflict,
     ExternalServiceError, //TypedResults.Problem
-    BusinessError, //BadRequest
+    RuleViolation, //BadRequest
     InternalError, //internal server error
     PaymentRequired,
     Unauthorized,
