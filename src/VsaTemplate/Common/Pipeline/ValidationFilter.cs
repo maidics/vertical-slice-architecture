6d2b357be8cc15558cca354baf67ba5be9b62f7e
maidics <your-username@users.noreply.github.com>
2026-08-12 09:40:43 +0200

    docs: modify ValidationFilter.cs comment for simplicity and clarity

--- src/VsaTemplate/Common/Pipeline/ValidationFilter.cs
+++ src/VsaTemplate/Common/Pipeline/ValidationFilter.cs
@@ -4,15 +4,8 @@ using VsaTemplate.Common.Interfaces.Features;
 
 namespace VsaTemplate.Common.Pipeline;
 
-/* This makes sense if:
-    - the request object is not created inside the minimal API method
-    - CRUD operations that would take advantage of this:
-        - Create: if there's a creation DTO (has to be validated: e.g.: registration) - without a DTO this is not required
-        - Update: if there's an update DTO (has to be validated: e.g.: change FullName prop on profile) -
-          even if one argument the request object should be passed in the HTTP body
-        - Read: same pattern
-        
-    If you want to validate the request inside or after the Minimal API you have to inject it's validator and validate manually.
+/* This makes sense if the request object is not created inside the minimal API method but comes with the incoming request
+   If you want to validate the request inside or after the Minimal API you have to inject its validator and validate manually.
 */
 
 public sealed class ValidationFilter : IEndpointFilter
@@ -47,35 +40,34 @@ public sealed class ValidationFilter : IEndpointFilter
 
         var validationContext = new ValidationContext<object>(request);
 
-        if (validators.Count != 0)
+        if (validators.Count == 0)
         {
-            var cancellationToken = context.HttpContext.RequestAborted;
+            return await next(context);
+        }
 
-            var validationResults = await Task.WhenAll(
-                validators.Select(v => v.ValidateAsync(validationContext, cancellationToken))
+        var cancellationToken = context.HttpContext.RequestAborted;
+
+        var validationResults = await Task.WhenAll(
+            validators.Select(v => v.ValidateAsync(validationContext, cancellationToken))
+        );
+
+        var failures = validationResults.Where(r => !r.IsValid).SelectMany(r => r.Errors).ToList();
+
+        if (failures.Count != 0)
+        {
+            var errorsDictionary = failures
+                .GroupBy(x => x.PropertyName)
+                .ToDictionary(g => g.Key, g => g.Select(x => x.ErrorMessage).ToArray());
+
+            _logger.LogWarning(
+                "Request validation failed: {HttpMethod} {Path}, {@UserId}, {@ValidationErrors}",
+                context.HttpContext.Request.Method,
+                context.HttpContext.Request.Path.Value,
+                _user.Id,
+                errorsDictionary
             );
 
-            var failures = validationResults
-                .Where(r => !r.IsValid)
-                .SelectMany(r => r.Errors)
-                .ToList();
-
-            if (failures.Count != 0)
-            {
-                var errorsDictionary = failures
-                    .GroupBy(x => x.PropertyName)
-                    .ToDictionary(g => g.Key, g => g.Select(x => x.ErrorMessage).ToArray());
-
-                _logger.LogWarning(
-                    "Request validation failed: {HttpMethod} {Path}, {@UserId}, {@ValidationErrors}",
-                    context.HttpContext.Request.Method,
-                    context.HttpContext.Request.Path.Value,
-                    _user.Id,
-                    errorsDictionary
-                );
-
-                return TypedResults.ValidationProblem(errorsDictionary);
-            }
+            return TypedResults.ValidationProblem(errorsDictionary);
         }
 
         return await next(context);
