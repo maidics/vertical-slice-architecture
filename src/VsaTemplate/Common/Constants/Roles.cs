5d67f3d0ff1b1fc1d21c72794f68ad320f94b863
maidics <your-username@users.noreply.github.com>
2026-08-31 13:58:59 +0200

    fix: add readonly to Roles.All

--- src/VsaTemplate/Common/Constants/Roles.cs
+++ src/VsaTemplate/Common/Constants/Roles.cs
@@ -7,7 +7,7 @@ public abstract class Roles
     public const string User = nameof(User);
     public const string Administrator = nameof(Administrator);
 
-    public static FrozenSet<string> All = [User, Administrator];
+    public static readonly FrozenSet<string> All = [User, Administrator];
 
     public static bool IsValid(string role) => All.Contains(role);
 }
