d2866bf5d09558765c6f38367267da5295fa8b0a
maidics <your-username@users.noreply.github.com>
2026-09-02 14:24:04 +0200

    chore: add scripts folder with pack-install scripts
    Exclude scripts from ci.yml and nuspec.csproj.

--- pack-install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-rm -rf ./artifacts/package/release/
-rm -rf ./scratch/
-dotnet pack nuspec.csproj
-dotnet new uninstall Vertical.Slice.Architecture
-dotnet new install ./artifacts/package/release/*.nupkg
